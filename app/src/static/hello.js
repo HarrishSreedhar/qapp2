1533e0b1fe6d3f71d00d7e6e69565eb444386b09
HarrishSreedhar <harrishsreedharen97@gmail.com>
2017-06-29 15:53:17 +0530

    yeap...back to normalgit add .

--- app/src/static/hello.js
+++ app/src/static/hello.js
@@ -1,9 +1,9 @@
-var express=require("express");
+/*var express=require("express");
 var app=express();
 app.get('/',function(req,res){
 	res.send("Heliiiii WOrld");
 });
 app.listen(8080,function(){
 	console.log("Listening to port 8080");
-});
-//console.log("Hello World");
+});*/
+console.log("Hello World");
