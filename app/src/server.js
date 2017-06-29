17f62f85b1237618197519a552271cc71699f3cf
HarrishSreedhar <harrishsreedharen97@gmail.com>
2017-06-29 20:27:49 +0530

    maaas ah?

--- app/src/server.js
+++ app/src/server.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 
 //your routes here
-app.get('/', function (req, res) {
-    res.send("Hello World!");
+app.get('/login', function (req, res) {
+	
+    res.send("Holaaaa!!!!");
 });
 
 app.listen(8080, function () {
